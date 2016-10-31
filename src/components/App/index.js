39d63eaddf359c4e7338150d8376f609455bd8c9
Maksim <mqklin@gmail.com>
2016-10-31 16:07:34 +0500

    init

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styles from './styles.scss';
+import classNames from 'classnames';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.root}>
+        root
+      </div>
+    );
+  }
+}
+
+export default App;
